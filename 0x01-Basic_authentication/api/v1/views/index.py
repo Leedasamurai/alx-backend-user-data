bc075ba1088ac5851531b196f6e71fe263aeca95
Leedasamurai <lesegoleemac@gmail.com>
2024-07-10 17:41:54 +0000

    error message forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized() -> None:
       - message unauthorized error
     """
     abort(401)
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """ GET /api/v1/forbidden
+    Return:
+      - message forbidden error
+    """
+    abort(403)
