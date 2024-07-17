6f35e6f8f0f95e5379fbdfd42b07a03a55946817
Leedasamurai <lesegoleemac@gmail.com>
2024-07-17 13:44:18 +0000

    add new user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -33,7 +33,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
         """
-        user = User(email=email, hashed_password=hashed_password)
+        new_user = User(email=email, hashed_password=hashed_password)
         try:
             self._session.add(new_user)
             self._session.commit()
